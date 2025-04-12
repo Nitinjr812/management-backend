f40aad07a3ab2a06ec007b87b5070fd028bd594c
Nitinjr812 <nitinjr812@gmail.com>
2025-04-12 11:36:40 +0530

    hiii

--- App.js
+++ App.js
@@ -15,9 +15,15 @@ mongoose.connect("mongodb+srv://nitin:Oio3pg0yQy4UQR8W@cluster0.lgmyvk0.mongodb.
 })
 
 
+
+
+
 app.post("/teacher", async (req, res) => {
     const users = req.body.user
 
+
+
+    
     let a = teacher({
         name: users.name,
         email: users.email,
